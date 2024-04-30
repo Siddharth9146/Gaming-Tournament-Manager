64348401718eab080ae3c9bf42b8c0f62b08b5a5
Ksshitij-Singare <130656366+Ksshitij-Singare@users.noreply.github.com>
2024-04-30 23:16:18 +0530

    Update tournamentModel.py

--- backend/model/tournamentModel.py
+++ backend/model/tournamentModel.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from typing import List
 
 class Tournament(BaseModel):
+    # add the name of tournament creator, which will be the person logging in.....
     tournamentName: str
     tournamentType: str
     tournamentParticipants: List[str] #usernames of the players in the tournament
