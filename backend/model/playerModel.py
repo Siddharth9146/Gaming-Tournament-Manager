753d4a95221efa79f6e5034f8806193f6587bb67
Ksshitij-Singare <130656366+Ksshitij-Singare@users.noreply.github.com>
2024-04-30 20:52:47 +0530

    Update playerModel.py

--- backend/model/playerModel.py
+++ backend/model/playerModel.py
@@ -5,5 +5,6 @@ class Player(BaseModel):
     username: str
     tournamentHistory: list[str] # list of tournament obj ids the player has been in
     matchHistory: List[str] # list of match obj ids the player has been in
+    #password is needed for tournament login page...... ksshitij
 
     #playerCollection.insert_one(dict(username="sid", tournamentHistory=[], matchHistory=[]))
