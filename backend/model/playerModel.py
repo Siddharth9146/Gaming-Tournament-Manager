71c3adf45aaf27e7e4864d551f716bc713b58af4
Siddharth9146 <rajopadhyesiddharth@gmail.com>
2024-05-03 01:41:52 +0530

    backend changes

--- backend/model/playerModel.py
+++ backend/model/playerModel.py
@@ -7,5 +7,5 @@ class Player(BaseModel):
     matchHistory: List[str] # list of match obj ids the player has been in
     #password is needed for tournament login page...... ksshitij
     #create password ka bhi soch, player registraion mein mein new password banara...
-
+    
     #playerCollection.insert_one(dict(username="sid", tournamentHistory=[], matchHistory=[]))
