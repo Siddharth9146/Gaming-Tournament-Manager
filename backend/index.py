828fb511bcccb620d83e913afda4fc9a3d5d675a
Siddharth9146 <rajopadhyesiddharth@gmail.com>
2024-06-04 17:10:57 +0530

    jhv

--- backend/index.py
+++ backend/index.py
@@ -32,7 +32,7 @@ origins = [
     "http://localhost:8080",
     "http://localhost:5000",
     "http://localhost:5500",
-    "http://127.0.0.1/"
+    "http://127.0.0.1:5500"
 ]
 app.add_middleware(
     CORSMiddleware,
--- frontend/js/tournamentLogin.js
+++ frontend/js/tournamentLogin.js
@@ -1,32 +1,64 @@
-// get tournament code and name from tournament_login.html and send to api '/checkTournamentCode/{tournamentCode}/{tournamentName}'
-// if tournament code and name are correct, redirect to match_creation.html
-// if not, show error message
+// // get tournament code and name from tournament_login.html and send to api '/checkTournamentCode/{tournamentCode}/{tournamentName}'
+// // if tournament code and name are correct, redirect to match_creation.html
+// // if not, show error message
 
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('tournament_login').addEventListener('submit', function(event){
         event.preventDefault();
-        const tournamentCode = document.getElementById('tournamentCode').value;
-        const tournamentName = document.getElementById('tournamentName').value;
+        const tournamentCode = document.getElementById('tournament_code').value;
+        const tournamentName = document.getElementById('tournament_name').value;
         checkTournamentCode(tournamentCode, tournamentName);
 
     }); 
 }
 );
 
-async function checkTournamentCode(tournamentCode, tournamentName){
-    try{
-        // Get player data
-        const response = await fetch(`/checkTournamentCode/${tournamentCode}/${tournamentName}`);
-        const data = await response.json();
-        if (data.success){
-            window.location.href = "/match_creation.html";
-        }
-        else{
-            document.getElementById("error").innerHTML = "Invalid tournament code or name";
-        }
-    }
-    catch(error){
-        console.log("Error in finding tournament");
-    }
-}
+// async function checkTournamentCode(tournamentCode, tournamentName){
+//     try{
+//         // Get player data
+//         const response = await fetch(`/checkTournamentCode/${tournamentCode}/${tournamentName}`);
+//         const data = await response.json();
+//         if (data.success){
+//             window.location.href = "/match_creation.html";
+//         }
+//         else{
+//             document.getElementById("error").innerHTML = "Invalid tournament code or name";
+//         }
+//     }
+//     catch(error){
+//         console.log("Error in finding tournament");
+//     }
+// }
         
+function submitForm(e) {
+    e.preventDefault();
+   
+   var myform =    document.getElementById("myform");
+    
+    var formData = new FormData(myform);
+  
+    fetch("https://show.ratufa.io/json", {
+      method: "POST",
+      body: formData,
+    })
+      .then(response => {
+      if (!response.ok) {
+        throw new Error('network returns error');
+      }
+      return response.json();
+    })
+      .then((resp) => {
+        let respdiv = document.createElement("pre");
+        respdiv.innerHTML = JSON.stringify(resp, null, 2);
+        myform.replaceWith(respdiv);
+        console.log("resp from server ", resp);
+      })
+      .catch((error) => {
+        // Handle error
+        console.log("error ", error);
+      });
+  }
+  
+  var myform = document.getElementById("myform");
+  
+  myform.addEventListener("submit", submitForm);
